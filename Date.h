16f6c2461f7a2f74c415831cbbeb55a1675ac645
Luis Manuel Rios <riostorresGamer@gmail.com>
2022-09-21 23:10:30 -0500

    Se agrega las validaciones para el día, mes y ano. Tambien se modifico la forma de convertir el dato a entero en el constructor

--- Date.h
+++ Date.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <cstring>
 
+using namespace std;
+
 class Date
 {
 private:
@@ -10,49 +12,39 @@ private:
     int year;
 
 public:
-    Date(std::string date) {
-        char dayMonthYear[50];
-        char* ptr;
+    Date(string date) {
+      char dayMonthYear[50];
+      char* ptr;
 
-        strcpy(dayMonthYear, date.c_str());
+      strcpy(dayMonthYear, date.c_str());
+      ptr = strtok(dayMonthYear, "/");
+      this->year = stoi(ptr);
 
-        ptr = strtok(dayMonthYear, "/");
-        // this->year = std::stoi(ptr);
-        this->year = sscanf(ptr, "%d", &ptr);
+      ptr = strtok(NULL, "/");
+      this->month = stoi(ptr);
 
-        ptr = strtok(NULL, "/");
-        // this->month = std::stoi(ptr);
-        this->month = sscanf(ptr, "%d", &ptr);
-
-        ptr = strtok(NULL, " ");
-        // this->day = std::stoi(ptr);
-        this->day = sscanf(ptr, "%d", &ptr);
+      ptr = strtok(NULL, " ");
+      this->day = std::stoi(ptr);
     }
 
-    void setDay(/*int day*/) {
-        this->day = day;
+    void setDay(int day) {
+      this->day = day;
     }
 
-    void setMonth(/*int month*/) {
-        if (this->month < 1 || this->month > 12) {
-            std::cout << "El mes no es valido.";
-        }
-        // this->month = month;
+    void setMonth(int month) {
+      this->month = month;
     }
 
-    void setYear(/*int year*/) {
-        if (this->year < 1 || this->year > 3000) {
-            std::cout << "El ano que ingreso no es valido."; // No sean mal pensados
-        }
-        // this->year = year;
+    void setYear(int year) {
+      this->year = year;
     }
 
     int getDay() {
-        return this->day;
+      return this->day;
     }
 
     int getMonth() {
-        return this->month;
+      return this->month;
     }
 
     int getYear() {
@@ -60,14 +52,42 @@ public:
     }
 
     void toString() {
-        if (this->year %400 == 0){
-            std::cout << "Ano bisiesto";
-        } else {
-            if(this->year %4 == 0){
-                std::cout << "Ano bisiesto";
-            } else {
-                std::cout << "Ano no bisiesto";
-            }
+      cout << this->year << "/" << this->month << "/" << this->day;
+
+      if (this->month == 2 && this->year %400 == 0 || this->year %4 == 0){
+        if (this->day < 1 || this->day > 29){
+          cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
         }
+      } else if (this->month == 2){
+        if (this->day < 1 || this->day > 28){
+          cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+        }
+      } else if (this->month == 1 || this->month == 3 || this->month == 5 || this->month == 7 || this->month == 8 || this->month == 10 || this->month == 12){
+        if (this->day < 1 || this->day > 31){
+          cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+        }
+      } else if (this->month == 4 || this->month == 6 || this->month == 9 || this->month == 11){
+        if (this->day < 1 || this->day > 30){
+          cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+        }
+      }
+
+      if (this->year < 1 || this->year > 3000) {
+        cout << "\nEl ano que ingreso (" << this->year << ") no es valido.\n";
+      }
+      
+      if (this->month < 1 || this->month > 12) {
+        cout << "\nEl mes que ingreso (" << this->month << ") no es valido.\n";
+      }
+      
+      if (this->year %400 == 0){
+        cout << "\nAno bisiesto.\n";
+      } else {
+        if(this->year %4 == 0){
+          cout << "\nAno bisiesto.\n";
+        } else {
+          cout << "\nAno no bisiesto.\n";
+        }
+      }
     }
 };
