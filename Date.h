94d63789e199225401c621d13e8422ab5ab37276
Luis Manuel Rios <riostorresGamer@gmail.com>
2022-10-02 21:43:51 -0500

    Se restructara el codigo en metodos y se le agrega un metodo para manipular un operador para incrementar los dias con sus respectivas validaciones, tambien si el usuario se equivoca corrige la fecha

--- Date.h
+++ Date.h
@@ -10,6 +10,7 @@ private:
     int day;
     int month;
     int year;
+    bool validatedError = false;
 
 public:
     Date(string date) {
@@ -51,43 +52,109 @@ public:
         return this->year;
     }
 
-    void toString() {
-      cout << this->year << "/" << this->month << "/" << this->day;
-
+    int validatedDay() {
       if (this->month == 2 && this->year %400 == 0 || this->year %4 == 0){
-        if (this->day < 1 || this->day > 29){
+        if (this->day < 1) {
           cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+          this->day = 1;
+          this->validatedError = true;
+        } else if (this->day > 29) {
+          cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+          this->day = 1;
+          ++this->month;
+          this->validatedError = true;
         }
-      } else if (this->month == 2){
-        if (this->day < 1 || this->day > 28){
+      } else if (this->month == 2) {
+        if (this->day < 1) {
           cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+          this->day = 1;
+          this->validatedError = true;
+        } else if (this->day > 28) {
+          cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+          this->day = 1;
+          ++this->month;
+          this->validatedError = true;
         }
       } else if (this->month == 1 || this->month == 3 || this->month == 5 || this->month == 7 || this->month == 8 || this->month == 10 || this->month == 12){
-        if (this->day < 1 || this->day > 31){
+        if (this->day < 1) {
           cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+          this->day = 1;
+          this->validatedError = true;
+        } else if (this->day > 31) {
+          cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+          this->day = 1;
+          ++this->month;
+          this->validatedError = true;
         }
       } else if (this->month == 4 || this->month == 6 || this->month == 9 || this->month == 11){
-        if (this->day < 1 || this->day > 30){
+        if (this->day < 1) {
           cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+          this->day = 1;
+          this->validatedError = true;
+        } else if (this->day > 30) {
+          cout << "\nEl dia que ingreso (" << this->day << ") no es valido.\n";
+          this->day = 1;
+          ++this->month;
+          this->validatedError = true;
         }
       }
 
-      if (this->year < 1 || this->year > 3000) {
+      return 0;
+    }
+
+    int validatedYear() {
+      if (this->year < 1) {
         cout << "\nEl ano que ingreso (" << this->year << ") no es valido.\n";
+        this->year = 1;
+        this->validatedError = true;
+      } else if (this->year > 3000) {
+        cout << "\nEl ano que ingreso (" << this->year << ") no es valido.\n";
+        this->validatedError = true;
       }
-      
-      if (this->month < 1 || this->month > 12) {
+
+      return 0;
+    }
+
+    int validateMonth() {
+      if (this->month < 1) {
         cout << "\nEl mes que ingreso (" << this->month << ") no es valido.\n";
+        this->month = 1;
+        this->validatedError = true;
+      } else if (this->month > 12) {
+        cout << "\nEl mes que ingreso (" << this->month << ") no es valido.\n";
+        this->day = 1;
+        this->month = 1;
+        ++this->year;
+        this->validatedError = true;
       }
-      
-      if (this->year %400 == 0){
+
+      return 0;
+    }
+
+    int leapYear() {
+      if (this->year %400 == 0 && this->validatedError == false){
         cout << "\nAno bisiesto.\n";
       } else {
-        if(this->year %4 == 0){
+        if(this->year %4 == 0 && this->validatedError == false){
           cout << "\nAno bisiesto.\n";
-        } else {
+        } else if(this->validatedError == false) {
           cout << "\nAno no bisiesto.\n";
         }
       }
+
+      return 0;
+    }
+
+    Date& operator++() {
+      day++;
+      return *this;
+    }
+    
+    void toString() {
+      validateMonth();
+      validatedDay();
+      validatedYear();
+      leapYear();
+      cout << "\n" << this->year << "/" << this->month << "/" << this->day;
     }
 };
